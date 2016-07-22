8dae3bb30971e8a285adc85f4a1a8554a3e78cb4
Chad Montgomery <montgomery.chad.e@gmail.com>
2016-07-22 10:33:31 -0400

    added some basic conversion pipes

---
+++ app/pipes/meters-to-feet.pipe.ts
@@ -0,0 +1,7 @@
+import { Pipe, PipeTransform } from '@angular/core';
+@Pipe({name: 'metersToFeet'})
+export class MetersToFeetPipe implements PipeTransform {
+  transform(value: number): number {
+    return isNaN(value)?undefined:value* 3.280839895;
+  }
+}
---
+++ app/pipes/meters-to-miles.pipe.ts
@@ -0,0 +1,7 @@
+import { Pipe, PipeTransform } from '@angular/core';
+@Pipe({name: 'metersToMiles'})
+export class MetersToMilesPipe implements PipeTransform {
+  transform(value: number): number {
+    return isNaN(value)?undefined:value/1609.344;
+  }
+}
