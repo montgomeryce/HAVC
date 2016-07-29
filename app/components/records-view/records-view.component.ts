9b1456ff9774d29ec3090e731fe28cc1f8dfebf7
Chad Montgomery <montgomery.chad.e@gmail.com>
2016-07-29 18:27:34 -0400

    added top6 calorie burn chart and top 6 distance chart

--- app/components/records-view/records-view.component.ts
+++ app/components/records-view/records-view.component.ts
@@ -1,33 +1,34 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivityService}         from '../../services/activity.service';
 import {Activity} from "../../models/activity";
+import {BarChartComponent} from "../bar-chart/bar-chart.component";
 
 @Component({
     selector: 'records-view',
     moduleId: module.id,
     templateUrl: 'records-view.component.html',
     styleUrls: ['records-view.component.css'],
-    pipes: []
+    directives: [BarChartComponent]
 })
 
 export class RecordsViewComponent implements OnInit {
-    error:any;
-    trainingSessions:Activity[]
+    calBurnActs:Activity[];
+    longestActs:Activity[];
 
     constructor(private trainingService:ActivityService) {
 
     }
 
-    getListData() {
-        console.log('********* getListData **********');
-
-        this.trainingService.getActivities().then(
-            data => this.trainingSessions = data
+    getData() {
+        this.trainingService.getActivities('totalCalories','desc','6').then(
+            data => this.calBurnActs = data
+        );
+        this.trainingService.getActivities('totalDistance','desc','6').then(
+            data => this.longestActs = data
         );
     }
 
     ngOnInit() {
-        this.getListData();
+        this.getData();
     }
-
 }
