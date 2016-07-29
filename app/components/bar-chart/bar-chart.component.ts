7394d777fafec04afa7b86a877bbc9a2a4abac4d
Chad Montgomery <montgomery.chad.e@gmail.com>
2016-07-29 18:26:06 -0400

    added ability to scale chart data, and to specify output data

--- app/components/bar-chart/bar-chart.component.ts
+++ app/components/bar-chart/bar-chart.component.ts
@@ -14,24 +14,32 @@ export class BarChartComponent implements OnInit{
     private divs: any;
     private width: number;
     private height: number;
+    private metric: string;
     private host;
     private htmlElement: HTMLElement;
 
     constructor(private element: ElementRef,
+                @Attribute('metric') metric: string,
                 @Attribute('width') width: number,
                 @Attribute('height') height: number) {
+        this.metric = metric;
         this.width = width;
         this.height = height;
         this.htmlElement = this.element.nativeElement;
         this.host = d3.select(this.element.nativeElement);
     }
-    render(newValue) {
-        console.log('rendervalue:',newValue);
+
+    /**
+     *
+     * @param newValue
+     * @param scale: scale the data to the appropriate size. (1=100%)
+     */
+    render(newValue,scale) {
         if (!newValue) return;
 
         this.divs.data(newValue).enter().append('div')
             .transition().ease('elastic')
-            .style('width', d => d *.10 )
+            .style('width', d => d * scale )
             .text(d => Math.round(d));
 
     }
@@ -48,12 +56,22 @@ export class BarChartComponent implements OnInit{
             'height': this.height + 'px',
         }).
         selectAll('div');
-        this.render( this.sessions.map(d => d.totalCalories));
+        this.update();
 
     }
+
+    update(){
+        if(this.metric === 'totalCalories'){
+            this.render( this.sessions.map(d => d.totalCalories),.10);
+        }else if(this.metric === 'totalDistance'){
+            this.render( this.sessions.map(d => Math.round(d.totalDistance*0.000621371192237)),4);
+        }
+    }
+
+
     OnChanges() {
         console.log('changes');
-        this.render(this.sessions.map(d => d.totalCalories));
+        this.update();
 
     }
 }
